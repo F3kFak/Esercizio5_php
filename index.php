8808ce594f3d47835a0e71b2f7c8457a1c0d6bc5
F3kFak <ary.maggiorelli@gmail.com>
2022-10-25 12:31:17 +0200

    finito

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <form action="pag2.php" method="post">
+    <label>Nome Gioco</label>        
+    <input type="text" name="nomegioco" id="">
+    <label>Punteggio gioco</label>
+    <input type="number" name="punteggiogioco">
+    <input type="submit" name="bottone" value="invia">
+    </form>
+</body>
+</html>
---
+++ pag2.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <?php
+        session_start();
+        $nomegioco = $_POST['nomegioco'];
+        $punteggiogioco = $_POST['punteggiogioco'];
+        $_SESSION['o'] = false;
+
+        $_SESSION['ArrayGioco'] = $_SESSION['ArrayGioco'] ?? array(); // controlla se è nullo fai questo
+        $_SESSION['ArrayPunteggio'] = $_SESSION['ArrayPunteggio'] ?? array();
+
+        for ($i=0; $i <= count($_SESSION['ArrayGioco']); $i++) { 
+            if ($nomegioco == $_SESSION['ArrayGioco'][$i]) {
+                if ($punteggiogioco > $_SESSION['ArrayPunteggio'][$i]) {
+                    $_SESSION['ArrayPunteggio'][$i] = $punteggiogioco;
+                    $_SESSION['o'] = true;
+                } else {
+                    
+                }
+            } else {
+                
+            }
+        }
+
+        if (!$_SESSION['o']) {
+            array_push($_SESSION['ArrayGioco'], $nomegioco);
+            array_push($_SESSION['ArrayPunteggio'], $punteggiogioco);
+        }
+        
+        $ng = $_SESSION['ArrayGioco'];
+        $pg = $_SESSION['ArrayPunteggio'];
+
+        for($i = 0; $i < count($ng); $i++)
+        {
+            echo"nome: $ng[$i] ";
+            echo"punteggio: $pg[$i].<br>";
+        }
+    ?>
+
+        <form action="index.php" method="post">
+            <input type="submit" value="Ritorno" name="c">
+        </form>
+
+        <form action="pag3.php" method="post">
+            
+            <input type="submit" value="ok">
+        </form>
+
+</body>
+
+</html>
